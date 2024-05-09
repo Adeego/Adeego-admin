1fe6c9289eca290e2d4784946ffc85d685e5fbe2
Jeff-Maina <devyoungblud@gmail.com>
2024-05-09 23:15:14 +0300

    Add filtermenu ui

--- ADMIN/src/Pages/Products.jsx
+++ ADMIN/src/Pages/Products.jsx
@@ -9,6 +9,45 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ArrowUpDown, ListFilter, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+} from "@/components/ui/dropdown-menu";
+
+const FilterMenu = () => {
+  return (
+    <DropdownMenu className=' bg-red-200'>
+      <DropdownMenuTrigger className="outline-none">
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-[0.3rem] border-neutral-200/60 text-neutral-500"
+        >
+          <ListFilter size={15} strokeWidth={2} />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white  border-neutral-100 w-44 shadow-neutral-100 rounded-[0.5rem] mr-14">
+        <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem checked={true} onCheckedChange={() => {}}>
+          <small className="text-sm">All items</small>
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={() => {}} onCheckedChange={() => {}}>
+          <small className="text-sm">In stock</small>
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={() => {}} onCheckedChange={() => {}}>
+          <small className="text-sm">Out of stock</small>
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const Products = () => {
   const [addProduct, setAddProduct] = useState(false);
@@ -44,21 +83,20 @@ const Products = () => {
         </button>
       </div> */}
 
-      <header className="flex items-center justify-between px-2 gap-4">
+      <header className="flex items-center justify-between px-2 gap-2">
         <div className="w-full">
-          <Input className='rounded-[0.4rem] w-full border-neutral-200 focus:border-neutral-400 placeholder:text-neutral-500' placeholder='Search products' />
+          <Input
+            className="rounded-[0.4rem] w-full border-neutral-200/50 focus:border-neutral-400 placeholder:text-neutral-500"
+            placeholder="Search products"
+          />
         </div>
-        <div className="flex gap-2 shrink-0">
-          <Button
-            variant="outline"
-            size="icon"
-            className="rounded-[0.5rem] border-neutral-200 text-neutral-500"
-          >
-            <ListFilter size={15} strokeWidth={2} />
-          </Button>
+        <div className="flex gap-2 shrink-0 relative">
+          <div className="relative">
+            <FilterMenu />
+          </div>
           <Button
             size="icon"
-            className="rounded-[0.5rem] border-neutral-200 grid place-items-center bg-black text-white"
+            className="rounded-[0.3rem] border-neutral-200 grid place-items-center bg-black text-white"
           >
             <Plus size={15} strokeWidth={2} />
           </Button>
