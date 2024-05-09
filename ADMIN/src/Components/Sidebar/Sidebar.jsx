6f013f286057d11053bf710e70b80b52fc7569a9
Jeff-Maina <devyoungblud@gmail.com>
2024-05-09 21:04:02 +0300

    Add profile dropdown and sidebar sheet

---
+++ ADMIN/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { RiShoppingBag2Fill } from "react-icons/ri";
+import { HiMiniUsers } from "react-icons/hi2";
+import { MdDeliveryDining } from "react-icons/md";
+import logo from "../../Assets/icon.png";
+
+// Icons
+import { UserRound, LogOut, Bell, PanelLeft } from "lucide-react";
+
+// components
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import MobileSidebar from "./comps/MobileSidebar";
+
+// Profile Menu;
+const ProfileMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="outline-none">
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-full border-neutral-200 text-neutral-500"
+        >
+          <UserRound size={15} />
+        </Button>{" "}
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white !right-0 mr-2 rounded-[0.5rem] w-44 p-2">
+        <DropdownMenuLabel>Profile</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className="gap-2 text-neutral-700 flex justify-between">
+          <small className="text-sm">User Account</small>
+          <UserRound size={14} />
+        </DropdownMenuItem>
+        <DropdownMenuItem className="gap-2 text-neutral-700 flex justify-between">
+          <small className="text-sm">Notifications</small>
+          <Bell size={14} />
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="gap-2 text-neutral-700 flex justify-between">
+          <small className="text-sm">Logout</small>
+          <LogOut size={14} />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const Sidebar = () => {
+  const { pathname } = useLocation(); // Get the current pathname
+
+  if (pathname === "/login") {
+    return <div></div>;
+  }
+
+  return (
+    <>
+      {/* Navbar */}
+      <nav className="w-full h-14 border border-neutral-100 flex items-center justify-between p-2">
+        <div>
+          <MobileSidebar />
+
+        </div>
+        <div></div>
+        <div className="relative">
+          <ProfileMenu />
+        </div>
+      </nav>
+
+      {/* Sidebar */}
+      <div className="p-2 h-screen bg-LightGrey fixed top-0 left-0 hidden">
+        <div className="p-2 flex flex-col items-center h-full w-32 bg-white shadow-lg rounded-2xl ">
+          <div className="h-36 bg-white">
+            <img
+              src={logo}
+              alt="Adeego"
+              className="w-28 h-28 text-DeepGreen bg-DeepGreen rounded-2xl"
+            />
+          </div>
+          <div className="flex flex-col bg-white text-sm font-extrabold mb-7">
+            <Link
+              className={`p-2 no-underline flex flex-row items-center mb-3
+                    ${
+                      pathname === "/"
+                        ? "bg-DeepGreen text-LightGrey rounded-lg h-6"
+                        : "text-DarkGrey h-6 hover:bg-LightGrey rounded-lg"
+                    }`}
+              to={"/"}
+            >
+              <AiFillHome className="" />
+              <h3 className=" ml-1">Home</h3>
+            </Link>
+            <Link
+              className={`p-2 no-underline flex flex-row items-center mb-3
+                    ${
+                      pathname === "/products"
+                        ? "bg-DeepGreen text-LightGrey rounded-lg h-6"
+                        : "text-DarkGrey h-6 hover:bg-LightGrey rounded-lg"
+                    }`}
+              to={"/products"}
+            >
+              <RiShoppingBag2Fill />
+              <h3 className=" ml-1">Products</h3>
+            </Link>
+            <Link
+              className={`p-2 no-underline flex flex-row items-center mb-3
+                    ${
+                      pathname === "/orders"
+                        ? "bg-DeepGreen text-LightGrey rounded-lg h-6"
+                        : "text-DarkGrey h-6 hover:bg-LightGrey rounded-lg"
+                    }`}
+              to={"/orders"}
+            >
+              <MdDeliveryDining />
+              <h3 className=" ml-1">Orders</h3>
+            </Link>
+            <Link
+              className={`p-2 no-underline flex flex-row items-center mb-80
+                    ${
+                      pathname === "/customers"
+                        ? "bg-DeepGreen text-LightGrey rounded-lg h-6"
+                        : "text-DarkGrey h-6 hover:bg-LightGrey rounded-lg"
+                    }`}
+              to={"/customers"}
+            >
+              <HiMiniUsers />
+              <h3 className=" ml-1">Customers</h3>
+            </Link>
+            <Link
+              className={`p-2 no-underline flex flex-row items-center
+                    ${
+                      pathname === "/logout"
+                        ? "bg-DeepGreen text-LightGrey rounded-lg h-6"
+                        : "text-DarkGrey h-6 hover:bg-LightGrey rounded-lg"
+                    }`}
+              to={"/logout"}
+            >
+              <HiMiniUsers />
+              <h3 className=" ml-1">Logout</h3>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ ADMIN/src/Components/Sidebar/comps/MobileSidebar.jsx
@@ -0,0 +1,92 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import logo from "../../../Assets/icon.png";
+
+const links = [
+  {
+    label: "Dashboard",
+    icon: <Home size={20} strokeWidth={2} />,
+    pageLink: "/dashboard",
+  },
+  {
+    label: "Products",
+    icon: <ShoppingBag size={20} strokeWidth={2} />,
+    pageLink: "/products",
+  },
+  {
+    label: "Orders",
+    icon: <ShoppingCart size={20} strokeWidth={2} />,
+    pageLink: "/orders",
+  },
+  {
+    label: "Agents",
+    icon: <UsersRound size={20} strokeWidth={2} />,
+    pageLink: "/agents",
+  },
+];
+
+import {
+  Home,
+  PanelLeft,
+  ShoppingBag,
+  ShoppingCart,
+  UsersRound,
+} from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+
+const MobileSidebar = () => {
+  const location = useLocation();
+  const currentPage = location.pathname;
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        {" "}
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-[0.5rem] border-neutral-200 text-neutral-500"
+        >
+          <PanelLeft size={15} />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="bg-white text-left gap-10 flex flex-col"
+      >
+        <SheetHeader>
+          <div className="h-16 bg-white">
+            <img
+              src={logo}
+              alt="Adeego"
+              className="h-full text-DeepGreen bg-DeepGreen rounded-2xl"
+            />
+          </div>
+        </SheetHeader>
+        <div className="w-full flex flex-col">
+          {links.map((link, i) => {
+            return (
+              <Link to={link.pageLink}>
+                <div className="flex gap-4 items-center py-3 active:bg-neutral-100 rounded-xl px-2 ">
+                  <div className="text-neutral-600">{link.icon}</div>
+                  <p className="text-lg ">{link.label}</p>
+                </div>
+              </Link>
+            );
+          })}
+
+          {pathname}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
